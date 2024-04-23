593c5bda72d86d38f40bf9794a41bd5f2557f3b7
VicToriiAMC <162367950+VicToriiAMC@users.noreply.github.com>
2024-04-23 14:24:55 -0600

    Add files via upload

---
+++ Gameswebpage/Hangman - copia/Hangman - copia/scripts/word-list.js
@@ -0,0 +1,194 @@
+const wordList = [
+        {
+            word: "gitarra",
+            hint: "Instrumento musical con cuerdas."
+        },
+        {
+            word: "oxigeno",
+            hint: "Gas inoloro e incoloro esencial para la vida."
+        },
+        {
+            word: "volcan",
+            hint: "Gran elevacion natural de la superficie de la tierra."
+        },
+        {
+            word: "pintura",
+            hint: "Forma de arte que utiliza pintura para crear imagenes o expresiones."
+        },
+        {
+            word: "vanessa",
+            hint: "Nombre de la persona que elaboro este juego."
+        },
+        {
+            word: "futbol",
+            hint: "Popular deporte que se juega con un balon."
+        },
+        {
+            word: "chocolate",
+            hint: "Dulce bebida preparada a base de semillas de cacao."
+        },
+        {
+            word: "mariposa",
+            hint: "Insecto con alas de colores."
+        },
+        {
+            word: "historia",
+            hint: "Estudio de eventos pasado de la civilizacion humana."
+        },
+        {
+            word: "cafetera",
+            hint: "Maquina utilizada para crear una popular bebida caliente."
+        },
+        {
+            word: "escalera",
+            hint: "Herramienta utilizada para alcanzar cierta altura."
+        },
+        {
+            word: "camara",
+            hint: "Dispositivo ultilizado para grabar imagenes o videos."
+        },
+        {
+            word: "diamante",
+            hint: "Piedra preciosa conocida por su brillo y alto precio."
+        },
+        {
+            word: "aventura",
+            hint: "Una experiencia emocionante."
+        },
+        {
+            word: "esmeralda",
+            hint: "Piedra preciosa caracterizada por su color verde."
+        },
+        {
+            word: "bicicleta",
+            hint: "Medio de transporte de dos ruedas."
+        },
+        {
+            word: "atardecer",
+            hint: "Cuando el sol se oculta en el horizonte."
+        },
+        {
+            word: "baile",
+            hint: "Movimiento ritmico que suele acompañarse de musica."
+        },
+        {
+            word: "galaxia",
+            hint: "Un vasto sistema de estrellas, gas y rocas que se mantienen unidos por la gravedad."
+        },
+        {
+            word: "rubi",
+            hint: "Piedra preciosa caracterizada por el color rojo y por ser un nombre de mujer."
+        },
+        {
+            word: "arquitectura",
+            hint: "El arte y la ciencia de diseñar y construir edificios."
+        },
+        {
+            word: "bukele",
+            hint: "Apellido del presidente de un pais de Centro America."
+        },
+        {
+            word: "astronauta",
+            hint: "Persona entrenada para viajar y trabajar en el espacio."
+        },
+        {
+            word: "cascada",
+            hint: "Caida natural de agua."
+        },
+        {
+            word: "tecnologia",
+            hint: "Aplicacion del conocimiento de la ciencia para propositos practicos."
+        },
+        {
+            word: "arcoiris",
+            hint: "Fenomeno natural que suele aparecer en el cielo luego de la lluvia."
+        },
+        {
+            word: "universo",
+            hint: "Todo lo que existe incluyyendo el espacio y tiempo."
+        },
+        {
+            word: "vacacion",
+            hint: "Periodo de tiempo dedicado al placer o descanso."
+        },
+        {
+            word: "bosque",
+            hint: "Extendion territorial caracterizada por una densa biodiversidad."
+        },
+        {
+            word: "telefono",
+            hint: "Dispositivo usado para transmitir sonidos a largas distancias."
+        },
+        {
+            word: "lenguaje",
+            hint: "Sistema de comunicacion que usa palabras, gestos y sintaxis."
+        },
+        {
+            word: "desierto",
+            hint: "Extendion territorial caracterizada por poca o nula presipitaciones."
+        },
+        {
+            word: "girasol",
+            hint: "Flor de tallo largo y color amarillo."
+        },
+        {
+            word: "fantasia",
+            hint: "Genero que involucra la imaginacion, ficcion, magia y eventos supernaturales."
+        },
+        {
+            word: "telescopio",
+            hint: "Instrumento usado para ver cosas a largas distancias."
+        },
+        {
+            word: "sobrino",
+            hint: "Miembro de la familia."
+        },
+        {
+            word: "fotografia",
+            hint: "El arte, proceso o practica de crear imagenes usando un dispositivo electromagnetico."
+        },
+        {
+            word: "agua",
+            hint: "Recurso natural mas abundante en el planeta."
+        },
+        {
+            word: "balon",
+            hint: "Utilizado para jugar futbol."
+        },
+        {
+            word: "tropical",
+            hint: "clima predominante en Centro y Sur America."
+        },
+        {
+            word: "misterioso",
+            hint: "Dificil o imposible de entender, identificar o explicar."
+        },
+        {
+            word: "enigma",
+            hint: "Algo misterioso, dificil de resolver o entender."
+        },
+        {
+            word: "susurrar",
+            hint: "Hablar muy bajo o de forma secreta."
+        },
+        {
+            word: "sombra",
+            hint: "Ocurre cuando un objeto bloquea la luz directa."
+        },
+        {
+            word: "secreto",
+            hint: "Algo que se mantiene escondido o desconocido para algunos."
+        },
+        {
+            word: "curiosidad",
+            hint: "Un fuerte deseo de conocer o aprender algo."
+        },
+        {
+            word: "agotado",
+            hint: "Sensacion luego de una larga jornada de actividad fisica."
+        },
+        {
+            word: "laberinto",
+            hint: "Lugar donde solo existen varios caminos pero solo una salida."
+        },
+    ];
